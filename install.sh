1f427db79110e2a985a3a0541817d6cc9189d4dc
Alain00 <alain00.alvarez@gmail.com>
2022-01-09 19:45:25 -0500

    feat: adding optional gh cli installation

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
 QPR_INSTALL_FOLDER=$HOME/.sh-quick-pr
 SCRIPT_FILE=$QPR_INSTALL_FOLDER/sh-quick-pr.sh
+
+if ! command -v gh > /dev/null; then
+  if ! echo "$OSTYPE" | grep "linux" -q; then
+    read -p "You don't have gh cli installed, do want to intall it now (y/n)? " ANS
+    if [ "$ANS" != "${ANS#[Yy]}" ]; then
+      curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+      sudo apt update
+      sudo apt install gh
+    else
+      echo "🙄 Ok, just remember to install it later"
+    fi
+  else
+    echo "🚨 Remember to install gh cli in order to use this tool"
+  fi
+fi
 
 if [ ! -d $QPR_INSTALL_FOLDER ]; then
   git clone git@github.com:Alain00/sh-quick-pr.git $QPR_INSTALL_FOLDER
@@ -22,5 +39,4 @@ else
   echo "export QPR_INSTALL_FOLDER=${QPR_INSTALL_FOLDER}" >> $BASH_CONFIG
   echo $INSTALL_LINE >> $BASH_CONFIG
   echo "🥳 quick PR installed in $BASH_CONFIG"
-  echo "Please restart the shell to use the command"
 fi
